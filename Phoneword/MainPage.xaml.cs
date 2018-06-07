7eb694f9beb67b3c1ffd46a44a73954b27ec89a8
josephrennieCTS <32045@corbytechnicalschool.org>
2018-06-07 08:54:15 +0100

    7-6-18

--- Phoneword/MainPage.xaml.cs
+++ Phoneword/MainPage.xaml.cs
@@ -14,10 +14,12 @@ namespace Phoneword
 
         void OnTranslate(object sender, EventArgs e)
         {
+            //Take the phoneNumberText from the text box and send to
+            //toNumber function in the PhoneWordTranslator
             translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
             if (!string.IsNullOrWhiteSpace(translatedNumber))
             {
-                callButton.IsEnabled = true;
+                callButton.IsEnabled = true; //Enable call button
                 callButton.Text = "Call " + translatedNumber;
             }
             else
@@ -27,16 +29,21 @@ namespace Phoneword
             }
         }
 
+        //function to run whenever the call button is pressed
         async void OnCall(object sender, EventArgs e)
         {
+            //await response
             if (await this.DisplayAlert(
                     "Dial a Number",
                     "Would you like to call " + translatedNumber + "?",
                     "Yes",
                     "No"))
             {
+                //get IDialer servince and assign dialer to variable
                 var dialer = DependencyService.Get<IDialer>();
+                //so long as dialer has content...
                 if (dialer != null)
+                    //add number to PhoneNumbers list
                 {
                     App.PhoneNumbers.Add(translatedNumber);
                     callHistoryButton.IsEnabled = true;
